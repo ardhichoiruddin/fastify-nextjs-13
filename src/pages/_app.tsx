c906995b4f1a783ed7db210fe8b39f9afd696909
Ardhi Choiruddin <33990014+ardhichoiruddin@users.noreply.github.com>
2024-02-24 00:18:58 +0700

    replace material-tailwind to nextui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "@material-tailwind/react";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
+    <NextUIProvider>
       <Component {...pageProps} />
-    </ThemeProvider>
+    </NextUIProvider>
   );
 }
